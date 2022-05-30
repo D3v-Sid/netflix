d90c487dc78d409d8cc02a6238fcd4fdb05306a7
D3v-Sid <Dev-Sid@protonmail.com>
2022-05-30 22:03:30 +0200

    Feat : Firestore OK

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app";
 import { GlobalStyles } from "./global-style";
+import { firebase } from "./lib/firebase.prod";
+import { createContext } from "react";
+export const FirebaseContext = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<>
-		<GlobalStyles />
-		<App />
+		<FirebaseContext.Provider value={{ firebase }}>
+			<GlobalStyles />
+			<App />
+		</FirebaseContext.Provider>
 	</>
 );
